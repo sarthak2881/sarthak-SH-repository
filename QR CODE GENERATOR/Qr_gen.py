ff4975963f5b340ffceffe809d3d7d6e2889f816
Sarthak Sharma <62142704+sarthakk-sharma@users.noreply.github.com>
2022-09-04 11:15:51 +0530

    Add files via upload

---
+++ QR CODE GENERATOR/Qr_gen.py
@@ -0,0 +1,11 @@
+import qrcode as qr
+name=input('Enter a name : ')
+img=qr.make(f"Hello {name} , You've scanned the QR successfully ")
+img.save("QR CODE.png")
+from PIL import Image
+
+qrc=qr.QRCode(version=5,error_correction=qr.ERROR_CORRECT_H,box_size=5,border=4)
+qrc.add_data('https://github.com/sarthakk-sharma')
+qrc.make(fit=False)
+img=qrc.make_image(fill_color='#232e17')
+img.save('Sarthak_Github.png')
